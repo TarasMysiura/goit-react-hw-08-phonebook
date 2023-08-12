4345b6e657b19a9cf8a4e9eb6b383c1a07416761
TarasMysiura <tarasmysiura@gmail.com>
2023-08-13 00:03:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={<p>loading...</p>} persistor={persistor}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <BrowserRouter
+        basename="/goit-react-hw-08-phonebook"
+      >
         <App />
       </BrowserRouter>
     </PersistGate>
